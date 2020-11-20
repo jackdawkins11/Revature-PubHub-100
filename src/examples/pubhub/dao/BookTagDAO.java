28921c0e16088c12e5b409d053ccc516d54cb737
Jack Dawkins <jackdawkins1974@gmail.com>
2020-11-20 16:03:41 -0600

    Changed BookTagDAO to use BookTag object instead of String

--- src/examples/pubhub/dao/BookTagDAO.java
+++ src/examples/pubhub/dao/BookTagDAO.java
@@ -3,6 +3,7 @@ package examples.pubhub.dao;
 import java.util.List;
 
 import examples.pubhub.model.Book;
+import examples.pubhub.model.BookTag;
 
 /*
  * Interface to a Data Access Object handling queries related to book tags.
@@ -12,25 +13,25 @@ public interface BookTagDAO {
 	/*
 	 * Adds a tag to a book. Returns boolean indicating success.
 	 */
-	public boolean addBookTag( Book book, String tagName );
-	public boolean addBookTag( String isbn, String tagName );
+	public boolean addBookTag( Book book, BookTag bookTag );
+	public boolean addBookTag( String isbn, BookTag bookTag );
 
 	/*
 	 * Removes a tag from a book. Returns boolean indicating success.
 	 */
-	public boolean removeBookTag( Book book, String tagName );
-	public boolean removeBookTag( String isbn, String tagName );
+	public boolean removeBookTag( Book book, BookTag bookTag );
+	public boolean removeBookTag( String isbn, BookTag bookTag );
 	
 	/*
 	 * Gets all the tags for a given book.
 	 */
-	List<String> getTagsByBook( Book book );
-	List<String> getTagsByBook( String isbn );
+	List<BookTag> getTagsByBook( Book book );
+	List<BookTag> getTagsByBook( String isbn );
 	
 	/*
 	 * Gets all books for a given tag.
 	 */
-	List<Book> getBooksByTag( String tagName );
+	List<Book> getBooksByTag( BookTag bookTag );
 
 	
 }
